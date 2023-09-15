0c4e2d40a673fb688dc963842d5e02dec825c458
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2023-09-15 17:52:40 -0300

    Created the User class

---
+++ src/main/java/com/ferraz_dev/userdept/entities/User.java
@@ -0,0 +1,12 @@
+package com.ferraz_dev.userdept.entities;
+
+public class User {
+    // Declare all private attributes of the user
+    private Long id;
+    private String name;
+    private String email;
+
+
+    // access the department class and asociate it to the user
+    private Department department;
+}
