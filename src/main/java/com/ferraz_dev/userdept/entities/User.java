a49d1d88ddae9ed0c4ea60fd8c82faeb75c850c0
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2023-09-15 18:00:31 -0300

    User methods created.

--- src/main/java/com/ferraz_dev/userdept/entities/User.java
+++ src/main/java/com/ferraz_dev/userdept/entities/User.java
@@ -9,4 +9,43 @@ public class User {
 
     // access the department class and asociate it to the user
     private Department department;
+
+    // Constructor
+    public User() {
+
+    }
+
+    // Get/Set methods for the User instance
+    public Long getId() { 
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() { 
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
 }
