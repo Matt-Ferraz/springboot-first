56ce7f316602a6c0e90168527774fc4317e0d98d
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2023-09-15 18:38:21 -0300

    Added table on DB and Foreign key to user with Department.

--- src/main/java/com/ferraz_dev/userdept/entities/User.java
+++ src/main/java/com/ferraz_dev/userdept/entities/User.java
@@ -1,13 +1,28 @@
 package com.ferraz_dev.userdept.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
     // Declare all private attributes of the user
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
 
 
     // access the department class and asociate it to the user
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 
     // Constructor
