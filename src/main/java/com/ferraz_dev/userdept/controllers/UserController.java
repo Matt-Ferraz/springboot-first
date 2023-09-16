cdcbf50261578ee4a0356fbfcb7fc76a5dc292e4
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2023-09-16 14:47:02 -0300

    Added post method to create new users;

--- src/main/java/com/ferraz_dev/userdept/controllers/UserController.java
+++ src/main/java/com/ferraz_dev/userdept/controllers/UserController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController; 
 
@@ -35,5 +37,10 @@ public class UserController {
         
         User user = repository.findById(id).get();
         return user;
+    }
+
+    @PostMapping
+    public User insertUser(@RequestBody User user) {
+        return repository.save(user);
     }
 }
