be3b9096101cb62430ee78a2afbb9b8d4d486fa8
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2023-09-16 13:46:22 -0300

    New method to search for a user by their ID

--- src/main/java/com/ferraz_dev/userdept/controllers/UserController.java
+++ src/main/java/com/ferraz_dev/userdept/controllers/UserController.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; 
 
 // RestController will set up the class to respond requisitions
 // RequestMapping will set up the route
@@ -27,5 +28,12 @@ public class UserController {
         // return allUserResult;
         // shorter way to
         return repository.findAll();
+    }
+    @GetMapping(value = "/{id}")
+    public User getById(@PathVariable Long id) {
+        if(id == null) throw new IllegalArgumentException();
+        
+        User user = repository.findById(id).get();
+        return user;
     }
 }
